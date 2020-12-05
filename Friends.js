7105d6f84ab20053d7b3f18536ef3d33fde07486
whransom <70912451+whransom@users.noreply.github.com>
2020-12-05 08:38:03 +0500

    Add files via upload
    Suggested friends backend and styling changes ticket #125 and #128

--- Friends.js
+++ Friends.js
@@ -1,48 +1,135 @@
-import React, {Component, useEffect, useState} from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
 import '../post/feed'
 import './Friends.css'
-import Feed from '../post/feed';
+import axiosConfig from 'axios';
+import Axios from 'axios';
+import React, { Component } from "react";
+import 'semantic-ui-css/semantic.min.css'
+import "./App.css";
+
+const Person = ({ img, name, occupation, children }) => {
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return (
+    <div className="person">
+      <img src={url} alt="person img" />
+      <div>
+        <h4>{name}</h4>
+        <h4>{occupation}</h4>
+        {children}
+      </div>
+    </div>
+  );
+};
+const PersonList = () => {
+   
+   
+  return (
+   
+    <section className="person-list" >
+        <h1 className="person-list">Suggested Friends:</h1>
+      <Person img="34" name="john" >
+      <button class="mini ui button"  >
+  <i class="icon user"></i>
+  Add Friend
+</button>
+         </Person>
+      <Person img="22" name="bob" >
+      
+      <button class="mini ui button" >
+  <i class="icon user"></i>
+  Add Friend
+</button>
+
+      </Person>
+      <Person img="56" name="david"  >
+      <button class="mini ui button">
+  <i class="icon user"></i>
+  Add Friend
+</button>
+         </Person>
+    </section>
+  );
+};
 
 class friends extends React.Component{
 
-    componentDidMount(){
+  
+    constructor(props) {
+        super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
+        this.state = { //state is by default an object
+           friends: {},
+           response: {},
+           friendNum: '',
+           message: '',           
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+     }
 
-    }
+  componentWillMount() {
+   Axios('http://localhost:5000/friends/get_friends', {method: "GET"}).then(response => this.setState({friends: response.data}));
+   console.log(this.state.friends)
+  }
+      
+     
+  handleChange(event) { 
+   this.setState({friendNum: event.target.value}); 
+   Axios('http://localhost:5000/friends/get_friends', {method: "GET"}).then(response => this.setState({friends: response.data}));
+   console.log(this.state.message)
+   console.log(this.state.friends)
+  }
+  handleSubmit(event) {
+   event.preventDefault();
 
-    componentWillUnmount(){
-        
-    }
+     axiosConfig.post('http://127.0.0.1:5000/friends/addfriend',{
+      body : this.state.friendNum,
+      headers:{"Content-Type":"application/json",},
+      cache: "no-cache",
+      })
+      .then(response => {
+          let res = response.data
+          console.log(res)
+          this.setState({message: res['state']})
+          alert(this.state.message)
+          console.log(this.state.message)
+          console.log(this.state.friends)
+      });
+  
+  }
 
     render() {
-        return (
-        <div>
-            <div className="rows">
-                {/* <div className="row">
-                    <img className="image" alt="profilepic" src="https://images.unsplash.com/photo-1514218698632-ef079aeae842?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"></img>
-                    <h2 className="name">Marcos Quan</h2>
-                    <h4 className="age">Age: ~78</h4>
-                    <h4 className="height">Height: 5'9</h4>
-                    <h4 className="weight">Weight: ~147</h4>
-                    <h4 className="gender">Gender: Male</h4>
-                    <h4 className="number">Phone Number: 911</h4>
-                    <h4 className="email">Email: marcos@teamfit.com</h4>
-                </div> */}
-                <div className="row">
-                    <img className="image" alt="profilepic" src="https://images.unsplash.com/photo-1514218698632-ef079aeae842?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"></img>
-                    <h2 className="name">Marcos Quan</h2>
-                   <h4 className="number">Phone Number: 911</h4>
-                    <h4 className="email">Email: marcos@teamfit.com</h4>
-                </div>
+       const folks = this.state.friends
+      return (
+            <div >               
+               <h2 className='title'>Friends:</h2>
                
+               
+               <PersonList />;
+               
+               
+               
+               {Object.keys(folks).map((key,id) => {
+                  return <h2 key={id} className="friend">{this.state.friends[key]} | {key}</h2>
+               })}
               
-               
-            </div>
-        </div>
-        )
+              <form onSubmit={this.handleSubmit}>
+            <label style={{ fontWeight: 'bold' }}>
+               Add a friend: 
+               <input type="text" value={this.state.friendNum} onChange={this.handleChange}/>
+            </label>
+            <input type="submit" value="Submit"/>
+            </form>  
+            </div>      
+         )
+         
+            
+          
+         
     };
+    
+     
 }
 
 
-
+// ReactDOM.render(<friends />, document.getElementById('root'));
 export default friends;
